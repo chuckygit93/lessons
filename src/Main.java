3a6543d9cbc58a18e7596b94161b2ad26e086c3c
gtambovtsev <gtambovtsev@inpolus.ru>
2023-11-29 18:58:49 +0300

    less than 100

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         int number = 6;
-        if (number > 0) {
+        if (number > 0 && number < 100) {
             System.out.println("!!!!");
         }
     }
