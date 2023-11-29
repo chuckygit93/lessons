14e040a9a2e63d03225021568bd91d9092675db5
chuckygit93 <roshin.chuckyisgod@yandex.ru>
2023-11-29 19:13:02 +0300

    add if number == 40

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
+     //Если number == 40, то вывести в консоль "number = 40"
     public static void main(String[] args) {
         System.out.println("Hello");
         int number = 6;
         if (number > 0 && number != 40 && number < 100) {
             System.out.println("!!!!");
+        } else if (number == 40) {
+            System.out.println("number ="+ number);
+
         }
     }
 }
