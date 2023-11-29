db17c7c78824db876726fe25de8a07dc5b834129
chuckygit93 <roshin.chuckyisgod@yandex.ru>
2023-11-29 18:38:50 +0300

    if number more than zero

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        int number = 6;
+        if (number > 0) {
+            System.out.println("!!!!");
+        }
     }
 }
